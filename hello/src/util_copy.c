68794dc421918c8483e248b79b44b7c4b9f45b9e
msoodb <masoud.bolhassani@gmail.com>
2021-03-14 01:01:45 +0200

    Makefiles

--- hello/src/util_copy.c
+++
@@ -1,8 +0,0 @@
-// hello.c
-#include <stdlib.h>
-#include <stdio.h>
- 
-int util(int argc) {
-	printf("argc %d\n", argc);
-	return 0;
-}
--- hello/util.c
+++
@@ -1,7 +0,0 @@
-#include <stdlib.h>
-#include <stdio.h>
- 
-int util(int argc) {
-	printf("argc %d\n", argc);
-	return 0;
-}
